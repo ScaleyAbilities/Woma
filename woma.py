af1d47950882fed77a7fca79e64c4d91caa43ad0
Morgan Zolob <mzolob@gmail.com>
2019-02-06 20:44:59 -0800

    Fix Woma issues and add some logging

--- woma.py
+++ woma.py
@@ -1,21 +1,27 @@
 import requests
 import sys
+import time
 
 API_ENDPOINT = 'http://127.0.0.1:80/api/command'
 
-fp = open('workload files/' + str(sys.argv[1]), 'r')
+num = 1
+
+program_time = time.perf_counter_ns()
+requests_time = 0
+
+# Use a session so that we keep the connection alive
+s = requests.Session()
+
+fp = open(sys.argv[1], 'r')
 # for each command line in the workload file, create JSON object and execute GET/POST to API
 line = fp.readline()
 while line:
-    command = line.split(' ')[1].split(',')
+    command = line.strip().split(' ')[1].split(',')
     # print(command)
     if command[0] == 'COMMIT_BUY' or command[0] == 'CANCEL_BUY' or command[0] == 'COMMIT_SELL' or command[0] == 'CANCEL_SELL' or command[0] == 'DISPLAY_SUMMARY':
         data = {
             'cmd': command[0],
-            'usr': command[1],
-            'params': {
-                'userid': command[1]
-            }
+            'usr': command[1]
         }
 
     elif command[0] == 'ADD':
@@ -23,7 +29,6 @@ while line:
             'cmd': command[0],
             'usr': command[1],
             'params': {
-                'userid': command[1],
                 'amount': command[2]
             }
         }
@@ -34,19 +39,27 @@ while line:
             'cmd': command[0],
             'usr': command[1],
             'params': {
-                'userid': command[1],
                 'stock': command[2]
             }
         }
 
-    elif command[0] == 'BUY' or command[0] == 'SELL' or command[0] == 'SET_BUY_AMOUNT' or command[0] == 'SET_BUY_TRIGGER' or command[0] == 'SET_SELL_AMOUNT' or command[0] == 'SET_SELL_TRIGGER':
+    elif command[0] == 'BUY' or command[0] == 'SELL' or command[0] == 'SET_BUY_AMOUNT' or command[0] == 'SET_SELL_AMOUNT':
         data = {
             'cmd': command[0],
             'usr': command[1],
             'params': {
-                'userid': command[1],
                 'stock': command[2],
                 'amount': command[3]
+            }
+        }
+
+    elif command[0] == 'SET_BUY_TRIGGER' or command[0] == 'SET_SELL_TRIGGER':
+        data = {
+            'cmd': command[0],
+            'usr': command[1],
+            'params': {
+                'stock': command[2],
+                'price': command[3]
             }
         }
 
@@ -54,7 +67,7 @@ while line:
         if len(command) == 2:
             data = {
                 'cmd': command[0],
-                'usr': command[1],
+                'usr': None,
                 'params': {
                     'filename': command[1]
                 }
@@ -64,7 +77,6 @@ while line:
                 'cmd': command[0],
                 'usr': command[1],
                 'params': {
-                    'userid': command[1],
                     'filename': command[2]
                 }
             }
@@ -81,12 +93,20 @@ while line:
                         or 'SET_SELL_AMOUNT' or 'CANCEL_SET_SELL' or 'SET_SELL_TRIGGER':
         # print(data)
         # print(requests.post(API_ENDPOINT, headers={'Content-Type': 'application/json'}, data=None, json=data))
-        requests.post(API_ENDPOINT, headers={'Content-Type': 'application/json'}, data=None, json=data)
+        request_timer = time.perf_counter_ns()
+        s.post(API_ENDPOINT, headers={'Content-Type': 'application/json'}, data=None, json=data)
+        requests_time += time.perf_counter_ns() - request_timer
     elif command[0] == 'QUOTE' or 'DUMPLOG' or 'DISPLAY_SUMMARY':
-        requests.get(API_ENDPOINT, data)
+        s.get(API_ENDPOINT, data)
     else:
         print('unexpected request...')
+
+    sys.stdout.write('Sent Request %d\r' % num)
+    num += 1
 
     line = fp.readline()
 
 fp.close()
+
+print('Total program time:     %dns' % (time.perf_counter_ns() - program_time))
+print('Time spent on requests: %dns' % requests_time)
