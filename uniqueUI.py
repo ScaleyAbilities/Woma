cf92f3af341c8c1d6efd50487e7eb73a0cd3d206
sam charles <c.sam.charles@gmail.com>
2019-03-29 15:13:28 -0700

    add unique id script

---
+++ uniqueUI.py
@@ -0,0 +1,34 @@
+import sys
+
+fp = open(sys.argv[1], 'r')
+# for each command line in the workload file, create JSON object and execute GET/POST to API
+line = fp.readline()
+output = set()
+hash_bucket = {}
+while line:
+    uname = line.strip().split(' ')[1].split(',')[1]
+    if "finalLOG" not in uname:
+        output.add(uname)
+    line = fp.readline()
+
+for name in output:
+    letter = name[0]
+    if letter in hash_bucket: 
+        hash_bucket[letter] = hash_bucket[letter] + 1
+    else:
+        hash_bucket[letter] = 1
+
+max = 0
+min = 100000000
+
+for name in hash_bucket:
+    if max < hash_bucket[name]:
+        max = hash_bucket[name]
+
+    if min > hash_bucket[name]:
+        min = hash_bucket[name]
+
+    print(name + " freq: " + str(hash_bucket[name]))
+
+print("min freq: " + str(min) + "\nmax freq: " + str(max))
+print("TOTAL NAMES: " + str(len(output)))
