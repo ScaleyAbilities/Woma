fa44de0c8f20796f5f9873e22bf47ebfff226eed
Morgan Zolob <mzolob@gmail.com>
2019-04-04 22:50:47 -0700

    Distribute better

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading;
@@ -7,6 +8,13 @@ using Newtonsoft.Json.Linq;
 
 namespace Woma
 {
+    class Command
+    {
+        public string Name;
+        public string Username;
+        public string[] Params;
+    }
+
     class Program
     {
         internal static int Instances = int.Parse(Environment.GetEnvironmentVariable("INSTANCES") ?? "1");
@@ -27,65 +35,90 @@ namespace Woma
                 Environment.Exit(1);
             }
 
-            var lines = File.ReadAllLines(filename);
+            Console.WriteLine("Loading Records...");
 
+            var commands = File.ReadAllLines(filename).Select(line => {
+                var command = new Command();
+                var firstComma = line.IndexOf(',');
+                var start = line.IndexOf(' ') + 1;
+                command.Name = line.Substring(start, firstComma - start).Trim();
+                command.Params = line.Substring(firstComma + 1).Split(',').Select(p => p.Trim()).ToArray();
+
+                if (command.Name != "DUMPLOG" || command.Params.Length > 1)
+                    command.Username = command.Params[0];
+
+                return command;
+            });
+
+            var usernames = new HashSet<string>();
+            foreach (var command in commands)
+            {
+                if (command.Username != null)
+                    usernames.Add(command.Username);
+            }
+
+            var usernameQueue = new Queue<string>(usernames);
+            var groupedUsernames = new HashSet<string>[Instances];
+            var index = 0;
+            while (usernameQueue.Any())
+            {
+                var username = usernameQueue.Dequeue();
+
+                if (groupedUsernames[index] == null)
+                    groupedUsernames[index] = new HashSet<string>();
+
+                groupedUsernames[index].Add(username);
+                index = (index + 1) % Instances;
+            }
+
+            var groupedCommands = new IEnumerable<Command>[Instances];
+            for (var i = 0; i < Instances; i++)
+            {
+                var localI = i;
+                groupedCommands[localI] = commands.Where(c => c.Username != null && groupedUsernames[localI].Contains(c.Username));
+            }
+
+            // Add any null usernames to the final group
+            groupedCommands[Instances - 1] = groupedCommands[Instances - 1].Concat(commands.Where(c => c.Username == null));
+
+            Console.WriteLine($"Loaded {usernames.Count()} usernames");
+            
             Console.WriteLine("Inserting Records...");
 
             var tasks = new Task[Instances];
-
             var inserted = new int[Instances];
 
             for (var i = 0; i < Instances; i++)
             {
                 var instance = i; // Fixes closure issues
                 tasks[instance] = Task.Run(() => {
-                    var instanceLines = lines.Where(l => l[l.IndexOf(',') + 1] % Instances == instance);
-
-                    foreach (var line in instanceLines.Select(l => l.Trim()))
-                    {
-                        var firstComma = line.IndexOf(',');
-                        var start = line.IndexOf(' ') + 1;
-                        var command = line.Substring(start, firstComma - start);
-                        var commandParams = line.Substring(firstComma + 1).Split(',');
-                        
-                        if (commandParams.Length < 1) {
-                            Console.WriteLine("Bad line" + line);
-                        }
-
+                    foreach (var command in groupedCommands[instance])
+                    {                        
                         var json = new JObject();
-                        json.Add("cmd", command);
+                        json.Add("cmd", command.Name);
+                        json.Add("usr", command.Username);
 
                         var jsonParams = new JObject();
 
-                        if (commandParams.Length > 0)
+                        if (command.Name == "DUMPLOG" && command.Params.Length == 1)
+                            jsonParams.Add("filename", command.Params[0]);
+
+                        if (command.Params.Length > 1)
                         {
-                            if (command == "DUMPLOG" && commandParams.Length == 1)
-                            {
-                                json.Add("usr", null);
-                                jsonParams.Add("filename", commandParams[0]);
-                            }
+                            if (command.Name == "ADD")
+                                jsonParams.Add("amount", command.Params[1]);
+                            else if (command.Name == "DUMPLOG")
+                                jsonParams.Add("filename", command.Params[1]);
                             else
-                            {
-                                json.Add("usr", commandParams[0]);
-                            }
+                                jsonParams.Add("stock", command.Params[1]);
                         }
 
-                        if (commandParams.Length > 1)
+                        if (command.Params.Length > 2)
                         {
-                            if (command == "ADD")
-                                jsonParams.Add("amount", commandParams[1]);
-                            else if (command == "DUMPLOG")
-                                jsonParams.Add("filename", commandParams[1]);
+                            if (command.Name == "SET_BUY_TRIGGER" || command.Name == "SET_SELL_TRIGGER")
+                                jsonParams.Add("price", command.Params[2]);
                             else
-                                jsonParams.Add("stock", commandParams[1]);
-                        }
-
-                        if (commandParams.Length > 2)
-                        {
-                            if (command == "SET_BUY_TRIGGER" || command == "SET_SELL_TRIGGER")
-                                jsonParams.Add("price", commandParams[2]);
-                            else
-                                jsonParams.Add("amount", commandParams[2]);
+                                jsonParams.Add("amount", command.Params[2]);
                         }
 
                         json.Add("params", jsonParams);
